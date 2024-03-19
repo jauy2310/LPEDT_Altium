907705b0d6836c2e47dfb3ef60b4a3db020263dc
Jake Uyechi <uyechi23@up.edu>
2024-03-19 14:07:49 -0600

    Modified push_changes batch script

--- push_changes.bat
+++ push_changes.bat
@@ -1,18 +1,24 @@
-:: turn command outputs off
+:: turn console feedback off
 @echo off
 
 :: remove the history directory (if it exists) in both folders
 rmdir .\LPHR_WeatherStationPCB\History\ /s /q
 rmdir .\LPEDT_Parts\History\ /s /q
 
-:: prompt user for commit message
-set [msg=[""]]
+:: prompt user for input
+:input_message
+set /p commit_message=Enter your commit message:
 
-:SETMSG    
-set /p "msg=Enter Commit Message: "
-IF ["%msg"==""] GOTO SETMSG
+if "%commit_message%"=="" (
+    echo Commit message cannot be empty. Please try again.
+    goto input_message
+)
 
 :: perform git upload
 git add .
-git commit -m "%msg"
+git commit -m "%commit_message%"
 git push -u origin main
+
+echo.
+echo.
+echo Commit Successful!
